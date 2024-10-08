2ebcee3684e32400edf79022035217e947ded815
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-10-08 18:29:09 +0700

    Три ковычки

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def start():
-    return "<!doctype html>"\
-        "<html>"\
-        "    <body>"\
-        "        <h1>web-сервер на flask</h1>"\
-        "    </body"\
-        "</html>"
+    return """<!doctype html>
+        <html>
+            <body>
+                <h1>web-сервер на flask</h1>
+            </body
+        </html>"""
+    

535c4a6d68a037d4faf48843c96248c360d3a86b
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-10-22 17:58:53 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect, abort
+from flask import Flask, url_for, redirect, abort, render_template
 from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
@@ -334,3 +334,6 @@ def add_flower(name):
     </body>
 </html>
 '''
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')

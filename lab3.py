0a9c298ab3867948f968ac74218e5b1eb7766009
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-11-06 00:59:25 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,11 +1,28 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, request, make_response
 lab3 = Blueprint('lab3', __name__)
 
 
 @lab3.route('/lab3/')
 def lab():
-    return render_template('lab3.html'), {'Accept-Language': 'ru-RU'}
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color')
+    return render_template('lab3.html', name=name, name_color=name_color), {'Accept-Language': 'ru-RU'}
+
 
 @lab3.route('/lab3/cookie')
 def cookie():
-    return 'cookie!!!'
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', 'magenta')
+    return resp
+
+
+@lab3.route('/lab3/del_cookie')
+def cookie_del():
+    resp=make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
+

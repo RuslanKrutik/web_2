2a8e16ceef291589acfd16c0b2b4d2739e9755b0
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-11-20 15:25:36 +0700

    Деление

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from werkzeug.exceptions import HTTPException
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
-
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 class PaymentRequired(HTTPException):
     code = 402
     description = "Требуется оплата."

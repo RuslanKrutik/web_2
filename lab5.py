b1009b62c443f0b0e7eef882af9316e75fb9c48a
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-12-12 16:12:13 +0700

    Улучшения

--- lab5.py
+++ lab5.py
@@ -7,6 +7,21 @@ lab5 = Blueprint('lab5', __name__)
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
+def db_connect():
+    conn = psycopg2.connect(
+        host = "127.0.0.1",
+        database = 'rus_krut_knowledge_base',
+        user = 'rus_krut_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+    
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
     if request.methods == 'GET':
@@ -18,25 +33,16 @@ def register():
     if not (login or password):
         return render_template('lab5/register.html', error='Заполните все поля')
     
-    conn = psycopg2.connect(
-        host = "127.0.0.1",
-        database = 'rus_krut_knowledge_base',
-        user = 'rus_krut_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor()
+    conn, cur = db_connect()
     
     cur.execute(f"SELECT login FROM users WHERE login='(login)';")
     if cur.fetchone():
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/register.html',
                             error="Такой пользователь уже существует")
         
     cur.execute(f"INSERT INTO users (login, password) VALUES('{login}', '{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
+    db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
 @lab5.route('/lab5/login', methods=['GET','POST'])

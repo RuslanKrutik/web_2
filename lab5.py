d8c563d4b1ae43afd9d4b92f44155f56482d102a
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-12-17 14:23:31 +0700

    Исправление 5 лабы

--- lab5.py
+++ lab5.py
@@ -10,7 +10,8 @@ def db_connect():
     db_path = path.join(dir_path, "database.db")
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
-    return conn
+    cur = conn.cursor()
+    return conn, cur
 
 def db_close(conn):
     conn.commit()

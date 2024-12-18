b658ce188466b4e9ae8af851de6d1022e6ba648a
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-12-18 16:15:17 +0700

    исправление

--- lab8.py
+++ lab8.py
@@ -3,7 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from db import db
 from db.models import users, articles
-from flask_login import login_user, login_required, current_user
+from flask_login import login_user, login_required, current_user, logout_user
 
 
 lab8 = Blueprint('lab8', __name__)

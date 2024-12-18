2c994f7b5d9f45a63c6b67f0aa79989b5f2bc055
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-12-18 13:14:23 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -4,13 +4,10 @@ import sqlite3
 from os import path
 from db import db
 from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 
 
 lab8 = Blueprint('lab8', __name__)
-
-@lab8.route('/lab8')
-def lab():
-    return render_template('lab8/lab8.html', login=session.get('login'))
 
 @lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
@@ -37,3 +34,36 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return redirect('/lab8/')
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+
+@lab8.route('/lab8/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+    
+    # Проверка на пустые поля
+    if not login_form:
+        return render_template('lab8/login.html', error="Имя пользователя не должно быть пустым")
+    if not password_form:
+        return render_template('lab8/login.html', error="Пароль не должен быть пустым")
+    
+    # Проверка существования пользователя
+    user = users.query.filter_by(login=login_form).first()
+    
+    # Проверка пароля
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember=False)
+            return redirect('/lab8/')
+        else:
+            return render_template('lab8/login.html', error="Ошибка входа: логин или пароль неверны")
+    else:
+        return render_template('lab8/login.html', error="Ошибка входа: пользователь не найден")
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "список статей"

0b0094dc6425c9060a9131e39c62fe89fae5e418
Крутиков Руслан <ruskakrutoj@gmail.com>
2024-12-18 11:32:25 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, url_for, redirect, abort, render_template
 import os
 from werkzeug.exceptions import HTTPException
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from os import path
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -13,6 +17,12 @@ from lab8 import lab8
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+dir_path = path.dirname(path.realpath(__file__))
+db_path = path.join(dir_path, "krutikov_orm.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
